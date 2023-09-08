28a11f483c5712108ce7b0227c9493a45f870d37
Soumya Tiwari <soumyatiwari7247@gmail.com>
2023-09-08 16:55:48 +0530

    fixed bugs

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import './Footer.css';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import "./Footer.css";
 
 const Footer = () => {
   // Replace these with your actual links
-  const linkedinLink = 'https://www.linkedin.com/in/soumyatiwari7247/';
-  const githubLink = 'https://github.com/soumyagithub2299';
-  const email = 'soumyatiwari7247@gmail.com';
-  const phoneNumber = '+91 7247092299';
+  const linkedinLink = "https://www.linkedin.com/in/soumyatiwari7247/";
+  const githubLink = "https://github.com/soumyagithub2299";
+  const email = "soumyatiwari7247@gmail.com";
+  const phoneNumber = "+91 7247092299";
 
   return (
+    // The Footer component consists of an AppBar, which is a Material-UI component.
     <AppBar position="static" color="primary">
       <Toolbar>
-        {/* Copyright and Footer Text */}
-        <Typography variant="body1" color="inherit">
+        {/* Left-aligned Typography displaying copyright information */}
+        <Typography variant="body1" color="inherit" className="footer-text">
           {new Date().getFullYear()} ©️ GYANODAYA
         </Typography>
 
-        {/* Social Media Icons */}
-        <div style={{ marginLeft: 'auto' }}>
-          {/* LinkedIn Icon */}
+        {/* Right-aligned section for social icons and contact information */}
+        <div style={{ marginLeft: "auto" }}>
+          {/* LinkedIn Icon Button */}
           <IconButton
             color="inherit"
             aria-label="LinkedIn"
@@ -33,17 +34,19 @@ const Footer = () => {
             target="_blank"
             rel="noopener noreferrer"
             style={{
-              fontSize: '2rem', // Increase icon size
-              transition: 'transform 0.3s', // Add transition
-              marginRight: '20px', // Increase space between LinkedIn and GitHub icons
+              fontSize: "2rem", // Increase icon size
+              transition: "transform 0.3s", // Add transition
+              marginRight: "20px", // Increase space between LinkedIn and GitHub icons
             }}
-            onMouseEnter={(e) => (e.currentTarget.style.transform = 'scale(1.1)')} // Scale up on hover
-            onMouseLeave={(e) => (e.currentTarget.style.transform = 'scale(1)')} // Reset on hover out
+            onMouseEnter={(e) =>
+              (e.currentTarget.style.transform = "scale(1.1)")
+            } // Scale up on hover
+            onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")} // Reset on hover out
           >
             <LinkedInIcon />
           </IconButton>
 
-          {/* GitHub Icon */}
+          {/* GitHub Icon Button */}
           <IconButton
             color="inherit"
             aria-label="GitHub"
@@ -52,20 +55,27 @@ const Footer = () => {
             target="_blank"
             rel="noopener noreferrer"
             style={{
-              fontSize: '2rem', // Increase icon size
-              transition: 'transform 0.3s', // Add transition
+              fontSize: "2rem",
+              transition: "transform 0.3s",
             }}
-            onMouseEnter={(e) => (e.currentTarget.style.transform = 'scale(1.2)')} // Scale up on hover
-            onMouseLeave={(e) => (e.currentTarget.style.transform = 'scale(1)')} // Reset on hover out
+            onMouseEnter={(e) =>
+              (e.currentTarget.style.transform = "scale(1.2)")
+            }
+            onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
           >
             <GitHubIcon />
           </IconButton>
-
-          {/* Contact Information */}
-          <Typography className="footer-text" variant="body2" color="inherit">
-            {email} | {phoneNumber}
-          </Typography>
         </div>
+
+        {/* Contact information with a margin top for spacing */}
+        <Typography
+          className="footer-text"
+          variant="body2"
+          color="inherit"
+          style={{ marginTop: "10px" }}
+        >
+          {email} | {phoneNumber}
+        </Typography>
       </Toolbar>
     </AppBar>
   );
